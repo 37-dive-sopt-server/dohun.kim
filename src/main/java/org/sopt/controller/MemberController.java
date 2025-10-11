aff820edd1c2b8c5c10f939caa17cb9f6fe213b1
Dohun Kim <dohun8758@naver.com>
2025-10-11 17:36:49 +0900

    feat: Member 기능 구현

---
+++ src/main/java/org/sopt/controller/MemberController.java
@@ -0,0 +1,23 @@
+package org.sopt.controller;
+
+import java.util.List;
+import java.util.Optional;
+import org.sopt.domain.Member;
+import org.sopt.service.MemberServiceImpl;
+
+public class MemberController {
+
+    private final MemberServiceImpl memberServiceImpl = new MemberServiceImpl();
+
+    public Long createMember(String name) {
+        return memberServiceImpl.join(name);
+    }
+
+    public Optional<Member> findMemberById(Long id) {
+        return memberServiceImpl.findOne(id);
+    }
+
+    public List<Member> getAllMembers() {
+        return memberServiceImpl.findAllMembers();
+    }
+}
