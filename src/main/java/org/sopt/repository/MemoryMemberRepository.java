82fe533069c56e1802a8165003f217bb90a36ee2
Dohun Kim <dohun8758@naver.com>
2025-10-15 19:36:07 +0900

    feat: MemberRepository 인터페이스로 정의 및 MemoryMemberRepository 구현체화
    - 추가적으로 existsById, existsByEmail, deleteById 메서드 추가

--- src/main/java/org/sopt/repository/MemoryMemberRepository.java
+++ src/main/java/org/sopt/repository/MemoryMemberRepository.java
@@ -7,25 +7,39 @@ import java.util.Map;
 import java.util.Optional;
 import org.sopt.domain.Member;
 
-public class MemoryMemberRepository {
+public class MemoryMemberRepository implements MemberRepository {
 
     private static final Map<Long, Member> store = new HashMap<>();
 
-
+    @Override
     public Member save(Member member) {
-
         store.put(member.getId(), member);
         return member;
-
     }
 
-
+    @Override
     public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
 
-
+    @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public Boolean existsById(Long memberId) {
+        return store.containsKey(memberId);
+    }
+
+    @Override
+    public Boolean existsByEmail(String email) {
+        return store.values().stream()
+                .anyMatch(member -> member.getEmail().equals(email));
+    }
+
+    @Override
+    public void deleteById(Long memberId) {
+        store.remove(memberId);
     }
 }
