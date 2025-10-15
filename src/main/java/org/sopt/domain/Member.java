6abef3869dfcda03eef8a5eb88bee98247d763cb
Dohun Kim <dohun8758@naver.com>
2025-10-15 19:32:58 +0900

    feat: Member에 이메일, 생년월일, 성별 필드 추가 및 정적 팩토리 메서드 사용을 통한 캡슐화
    - 추가적으로 성별은 Enum을 통해 구현(MALE, FEMALE, OTHER)

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -1,13 +1,28 @@
 package org.sopt.domain;
 
+import java.time.LocalDate;
+
 public class Member {
 
     private Long id;
     private String name;
+    private String email;
+    private Gender gender;
+    private LocalDate birthDate;
 
-    public Member(Long id, String name) {
+    private Member() {
+    }
+
+    private Member(Long id, String name, String email, Gender gender, LocalDate birthDate) {
         this.id = id;
         this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.birthDate = birthDate;
+    }
+
+    public static Member createOf(Long id, String name, String email, Gender gender, LocalDate birthDate) {
+        return new Member(id, name, email, gender, birthDate);
     }
 
     public Long getId() {
@@ -16,5 +31,17 @@ public class Member {
 
     public String getName() {
         return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
     }
 }
