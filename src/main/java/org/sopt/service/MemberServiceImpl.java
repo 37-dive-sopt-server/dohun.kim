0c677c510e81efbe4f1c0e881cb5acd830ee9700
Dohun Kim <dohun8758@naver.com>
2025-10-29 14:01:03 +0900

    feat: 회원 가입 및 조회 기능을 DTO 기반으로 수정

--- src/main/java/org/sopt/service/MemberServiceImpl.java
+++ src/main/java/org/sopt/service/MemberServiceImpl.java
@@ -1,14 +1,17 @@
 package org.sopt.service;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import org.sopt.domain.Gender;
 import org.sopt.domain.Member;
+import org.sopt.dto.MemberCreateRequest;
+import org.sopt.dto.MemberListResponse;
+import org.sopt.dto.MemberResponse;
 import org.sopt.exception.MemberErrorCode;
 import org.sopt.exception.MemberException;
 import org.sopt.repository.MemberRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
@@ -17,20 +20,29 @@ public class MemberServiceImpl implements MemberService {
         this.memberRepository = memberRepository;
     }
 
-    public Long join(String name, String email, Gender gender, LocalDate birthDate) {
-        if (memberRepository.existsByEmail(email)) {
+    public MemberResponse join(MemberCreateRequest memberCreateRequest) {
+        if (memberRepository.existsByEmail(memberCreateRequest.email())) {
             throw new MemberException(MemberErrorCode.DUPLICATE_EMAIL);
         }
-        Member savedMember = memberRepository.save(Member.createWithoutId(name, email, gender, birthDate));
-        return savedMember.getId();
+        Member savedMember = memberRepository.save(Member.createWithoutId(memberCreateRequest));
+        return savedMember.toMemberResponse();
     }
 
-    public Optional<Member> findOne(Long memberId) {
-        return memberRepository.findById(memberId);
+    public MemberResponse findOne(Long memberId) {
+        Optional<Member> foundMember = memberRepository.findById(memberId);
+        if (foundMember.isEmpty()) {
+            throw new MemberException(MemberErrorCode.MEMBER_NOT_FOUND);
+        }
+        return foundMember.get().toMemberResponse();
     }
 
-    public List<Member> findAllMembers() {
-        return memberRepository.findAll();
+    public MemberListResponse findAllMembers() {
+        List<Member> allMember = memberRepository.findAll();
+        return new MemberListResponse(
+                allMember.stream()
+                        .map(Member::toMemberResponse)
+                        .toList()
+        );
     }
 
     public void deleteMember(Long memberId) {
