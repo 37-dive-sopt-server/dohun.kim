9433308d4035f0552148e6f1c2b4c1f34c8fe82c
Dohun Kim <dohun8758@naver.com>
2025-10-29 22:53:56 +0900

    refactor: 에러코드의 예외 코드 변경 및 사용하지 않는 예외 코드 삭제
    - 1000번대 -> 맴버 입력값 관련 예외
    - 2000번대 -> 맴버 도메인 규칙 관련 예외

--- src/main/java/org/sopt/member/exception/MemberDomainErrorCode.java
+++ src/main/java/org/sopt/member/exception/MemberDomainErrorCode.java
@@ -4,10 +4,10 @@ import org.sopt.global.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum MemberDomainErrorCode implements ErrorCode {
-    DUPLICATE_EMAIL(HttpStatus.CONFLICT, 1002, "이미 존재하는 이메일입니다."),
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, 1003, "해당 ID의 회원을 찾을 수 없습니다."),
-    UNDERAGE_MEMBER(HttpStatus.FORBIDDEN, 1011, "20세 미만은 가입할 수 없습니다."),
-    MEMBER_REGISTRATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 1010, "회원 등록 실패");
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, 2000, "이미 존재하는 이메일입니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, 2001, "해당 ID의 회원을 찾을 수 없습니다."),
+    UNDERAGE_MEMBER(HttpStatus.FORBIDDEN, 2002, "20세 미만은 가입할 수 없습니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final int exceptionCode;
--- src/main/java/org/sopt/member/exception/MemberValidationErrorCode.java
+++ src/main/java/org/sopt/member/exception/MemberValidationErrorCode.java
@@ -4,16 +4,14 @@ import org.sopt.global.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum MemberValidationErrorCode implements ErrorCode {
-    EMPTY_NAME(HttpStatus.BAD_REQUEST, 1004, "이름을 입력해주세요."),
-    EMPTY_EMAIL(HttpStatus.BAD_REQUEST, 1007, "이메일을 입력해주세요."),
-    EMPTY_BIRTHDATE(HttpStatus.BAD_REQUEST, 1005, "생년월일을 입력해주세요."),
-    EMPTY_GENDER(HttpStatus.BAD_REQUEST, 1013, "성별을 입력해주세요."),
-    INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, 1001, "이메일 형식이 올바르지 않습니다."),
-    INVALID_NAME_FORMAT(HttpStatus.BAD_REQUEST, 1012, "이름은 숫자와 영어, 한글로만 구성될 수 있습니다."),
-    INVALID_BIRTHDATE(HttpStatus.BAD_REQUEST, 1014, "생년월일이 현재 날짜 이후일 수 없습니다."),
-    INVALID_BIRTHDATE_FORMAT(HttpStatus.BAD_REQUEST, 1006, "생년월일 형식이 올바르지 않습니다. (예: 2000-05-02)"),
-    INVALID_GENDER_INPUT(HttpStatus.BAD_REQUEST, 1008, "성별을 올바르게 입력해주세요 (MALE/FEMALE/OTHER)"),
-    INVALID_ID_FORMAT(HttpStatus.BAD_REQUEST, 1009, "유효하지 않은 ID 형식입니다. 숫자만 입력해주세요.");
+    EMPTY_NAME(HttpStatus.BAD_REQUEST, 1000, "이름을 입력해주세요."),
+    EMPTY_EMAIL(HttpStatus.BAD_REQUEST, 1001, "이메일을 입력해주세요."),
+    EMPTY_BIRTHDATE(HttpStatus.BAD_REQUEST, 1002, "생년월일을 입력해주세요."),
+    EMPTY_GENDER(HttpStatus.BAD_REQUEST, 1003, "성별을 입력해주세요."),
+    INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, 1004, "이메일 형식이 올바르지 않습니다."),
+    INVALID_NAME_FORMAT(HttpStatus.BAD_REQUEST, 1005, "이름은 숫자와 영어, 한글로만 구성될 수 있습니다."),
+    INVALID_BIRTHDATE(HttpStatus.BAD_REQUEST, 1006, "생년월일이 현재 날짜 이후일 수 없습니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final int exceptionCode;
