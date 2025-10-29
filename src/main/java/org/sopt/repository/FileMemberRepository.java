109dd189cead83afb531b534d9099fbc6045f673
Dohun Kim <dohun8758@naver.com>
2025-10-29 14:01:27 +0900

    feat: FileMemberRepository에 @Repository 어노테이션 추가

--- src/main/java/org/sopt/repository/FileMemberRepository.java
+++ src/main/java/org/sopt/repository/FileMemberRepository.java
@@ -6,7 +6,9 @@ import java.util.Map;
 import java.util.Optional;
 import org.sopt.domain.Member;
 import org.sopt.repository.storage.MemberFileStorage;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class FileMemberRepository implements MemberRepository {
 
     private static final String STORAGE_DIR_NAME = "data";
